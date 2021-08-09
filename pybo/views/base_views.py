bea1b324ea5be180729057ec110b92acbe58bcb9
김성현 <kimsh4265@korea.ac.kr>
2021-08-09 19:07:01 +0900

    delete direct log

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,14 +1,11 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, Count
-import logging
 
 from ..models import Question
 
-logger = logging.getLogger('pybo')
 
 def index(request):
-    logger.info("INFO 레벨로 출력")
 
     #pybo 목록 출력
 
